b3fe935faaad40b7f8473abf8500c7d27c7b075f
shark0der <commits@shark0der.com>
2022-12-07 20:42:53 +0200

    Mark constants and other members as internal

--- contracts/interfaces/IStakingPool.sol
+++ contracts/interfaces/IStakingPool.sol
@@ -115,13 +115,13 @@ interface IStakingPool {
 
   function manager() external view returns (address);
 
-  function getActiveStake() external view returns (uint);
+  // function getActiveStake() external view returns (uint);
 
-  function getProductStake(uint productId, uint coverExpirationDate) external view returns (uint);
+  // function getProductStake(uint productId, uint coverExpirationDate) external view returns (uint);
 
-  function getFreeProductStake(uint productId, uint coverExpirationDate) external view returns (uint);
+  // function getFreeProductStake(uint productId, uint coverExpirationDate) external view returns (uint);
 
-  function getAllocatedProductStake(uint productId) external view returns (uint);
+  // function getAllocatedProductStake(uint productId) external view returns (uint);
 
     /* ========== EVENTS ========== */
 
